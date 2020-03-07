ea7e9b0bd8f70f2589b0bc119476700f593633e5
86187 <86187@192.168.64.1>
2020-03-07 16:15:41 +0800

    nihao

---
+++ text/src/cn/tedu/com/Text01.java
@@ -0,0 +1,11 @@
+package cn.tedu.com;
+
+public class Text01 {
+
+	public static void main(String[] args) {
+
+System.out.println("nihao");
+
+	}
+
+}
---
+++ text/src/cn/tedu/com/Text02.java
@@ -0,0 +1,10 @@
+package cn.tedu.com;
+
+public class Text02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("haohaoxuexi ");
+	}
+
+}
